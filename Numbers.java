134a48399329fcca70c4e5aed283ddd2c22ae1d0
jmroman59 <65377746+jmroman59@users.noreply.github.com>
2020-05-14 20:52:17 -0500

    Jose R. Number practice

---
+++ Numbers.java
@@ -0,0 +1,16 @@
+public class Numbers {
+	
+	public static void main(String[] args){
+
+		int number1 = 5;
+		int number2;
+		number2 = 10;
+
+		int sum = number1 + number2;
+
+		System.out.println(sum);
+
+
+	}
+
+}
