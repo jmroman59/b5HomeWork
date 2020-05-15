03df6f15675ce7b01a785de64c15c887a67179ec
jmroman59 <65377746+jmroman59@users.noreply.github.com>
2020-05-14 20:50:19 -0500

    Jose R. Print Receipt Homework

---
+++ PrintReceipt.java
@@ -0,0 +1,17 @@
+public class PrintReceipt{
+	
+	public static void main(String[] args){
+		String itemOne = "Computer";
+		String itemTwo = "Headphones";
+		int costOne = 980;
+		int costTwo = 40;
+		int sum = costOne + costTwo;
+
+		System.out.println(itemOne+": "+costOne+" dollars");
+		System.out.println(itemTwo+": "+costTwo+" dollars");
+		System.out.println( );
+		System.out.println("Total: "+sum);
+
+	}
+
+}
